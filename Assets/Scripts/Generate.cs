5e642bb0f99d3544e1d019fbcd0765919fc3cc6a
고광웅 <ernham1@gmail.com>
2020-01-31 12:24:00 +0900

    프로젝트 파일 최초 업로드
    하이퍼 슬라이딩 프로젝트를 위한 파일 업로드

---
+++ Assets/Scripts/Generate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generate : MonoBehaviour
+{
+    //void GenerateRandomPuzzle(PuzzleParams* param)
+    //{
+    //    param->squares = new char[param->rows * param->cols];
+    //    for (int i = 1; i < param->rows * param->cols; i++)
+    //        param->squares[i - 1] = i;
+    //    param->emptySquare = param->rows * param->cols - 1;
+    //    param->squares[param->emptySquare] = 0;
+    //    for (int i = 0; i < param->rows * param->cols * 10; i++)
+    //        Move(param, (eMove)(rand() % 4));
+    //}
+
+}
