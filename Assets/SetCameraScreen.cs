4305aeab9a70ad88d3e9ce1b4f52e399a2af397b
Ryu Yong Gun <nauryu@gmail.com>
2020-03-09 17:26:56 +0900

    유니티 최신버전으로 변경, 안드로이드 테스트, UI 핵심 이슈 해결

---
+++ Assets/SetCameraScreen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCameraScreen : MonoBehaviour
+{
+    public Transform PuzzleTf;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Screen.SetResolution(720, 1280, true);
+
+        //Debug.Log("Screen Width : "+Screen.width);
+        //Debug.Log("Screen Height : " + Screen.height);
+
+        float screenRatio = 1280f / 720f;
+        float newScreenRatio = Screen.height*1f / Screen.width;
+
+        Debug.Log("screenRatio : " + screenRatio);
+        Debug.Log("newScreenRatio : " + newScreenRatio);
+
+
+        float changeScaleZ = screenRatio / newScreenRatio;
+        float changeScaleX =  newScreenRatio/ screenRatio;
+
+        Debug.Log("Change Sclale : "+ changeScaleZ);
+
+        PuzzleTf.localScale = new Vector3(1f, 1f, changeScaleZ);
+    }
+
+}
